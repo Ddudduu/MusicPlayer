cc338a4ca2187d36e98ad0849f466ccf5d294244
ddudduu <imjiwon@imjiwon-ui-noteubug.local>
2025-04-28 00:27:11 +0900

    feat : check read media, external storage permission

--- app/src/main/java/com/example/androidstudy/MainActivity.kt
+++ app/src/main/java/com/example/androidstudy/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.androidstudy
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -22,28 +29,46 @@ import androidx.compose.ui.unit.sp
 import com.example.androidstudy.data.Music
 import com.example.androidstudy.ui.theme.AndroidStudyTheme
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<com.example.androidstudy.viewModel.MusicViewModel>()
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show()
+            viewModel.getMusicList()
+        } else {
+            Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val musicList = listOf(
             Music("Come Over", "LE SSERAFIM"),
             Music("DENIAL IS A RIVER", "Doechii"),
             Music("Grapejuice", "Harry Styles")
         )
 
+//        setContent {
+//            AndroidStudyTheme {
+//                // A surface container using the 'background' color from the theme
+//                Column(
+//                    modifier = Modifier,
+////                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    AppTitle("Music Player🎧")
+//                    MusicList(musics = musicList)
+//                }
+//            }
+//        }
+
         setContent {
-            AndroidStudyTheme {
-                // A surface container using the 'background' color from the theme
-                Column(
-                    modifier = Modifier,
-//                    color = MaterialTheme.colorScheme.background
-                ) {
-                    AppTitle("Music Player🎧")
-                    MusicList(musics = musicList)
-                }
-            }
+            MainScreen(requestPermissionLauncher)
         }
     }
 }
@@ -97,6 +122,24 @@ fun MusicItem(
                 color = Color.DarkGray,
                 fontWeight = FontWeight.W300
             )
+        }
+    }
+}
+
+@Composable
+fun MainScreen(permissionLauncher: ActivityResultLauncher<String>){
+
+    Column{
+        Text("MP3 파일에 접근하려면 권한을 허용해주세요.")
+        Button(onClick = {
+            val permission = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                Manifest.permission.READ_MEDIA_AUDIO
+            }else{
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            }
+            permissionLauncher.launch(permission)
+        }){
+            Text("권한 요청")
         }
     }
 }
