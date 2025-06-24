983905bae64b1ad922b2a9ccb0d0097b8d69ffb2
Ddudduu <senta2006@naver.com>
2025-06-25 02:41:12 +0900

    feat: create PlayMusicUseCase and implement repository

---
+++ data/src/main/java/com/example/data/repository/PlayerRepositoryImpl.kt
@@ -0,0 +1,20 @@
+package com.example.data.repository
+
+import android.net.Uri
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import com.example.domain.repository.PlayerRepository
+import javax.inject.Inject
+
+class PlayerRepositoryImpl @Inject constructor(private val exoPlayer: ExoPlayer) :
+    PlayerRepository {
+    override fun play(path: String) {
+        exoPlayer.setMediaItem(MediaItem.fromUri(Uri.parse(path)))
+        exoPlayer.prepare()
+        exoPlayer.play()
+    }
+
+    override fun release() {
+        exoPlayer.release()
+    }
+}
---
+++ domain/src/main/java/com/example/domain/useCase/PlayMusicUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.domain.useCase
+
+import com.example.domain.repository.PlayerRepository
+import javax.inject.Inject
+
+class PlayMusicUseCase @Inject constructor(private val playerRepository: PlayerRepository) {
+    operator fun invoke(path: String) {
+        playerRepository.play(path)
+    }
+}
