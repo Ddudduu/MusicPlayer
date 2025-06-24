8d3d1d098fdac32cc5bed87c94a065d1a3437074
Ddudduu <senta2006@naver.com>
2025-06-25 01:43:01 +0900

    feat: add PlayerModule for DI

--- app/src/main/java/com/example/androidstudy/di/PlayerModule.kt
+++ app/src/main/java/com/example/androidstudy/di/PlayerModule.kt
@@ -1,19 +1,21 @@
 package com.example.androidstudy.di
 
 import android.content.Context
-import com.example.data.repository.MusicRepositoryImpl
-import com.example.domain.repository.MusicRepository
+import androidx.media3.exoplayer.ExoPlayer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule {
+object PlayerModule {
+
     @Provides
-    fun provideMusicRepository(@ApplicationContext context: Context): MusicRepository {
-        return MusicRepositoryImpl(context)
+    @Singleton
+    fun provideExoPlayer(@ApplicationContext context: Context): ExoPlayer {
+        return ExoPlayer.Builder(context).build()
     }
 }
