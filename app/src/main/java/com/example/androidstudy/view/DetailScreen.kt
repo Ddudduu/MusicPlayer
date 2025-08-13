4c36922b5a478c05e8378023705e421506fef56e
Ddudduu <senta2006@naver.com>
2025-08-14 01:15:54 +0900

    feat: set album cover from uri using MediaMetaDataRetriever (#7)

--- app/src/main/java/com/example/androidstudy/view/DetailScreen.kt
+++ app/src/main/java/com/example/androidstudy/view/DetailScreen.kt
@@ -1,5 +1,9 @@
 package com.example.androidstudy.view
 
+import android.graphics.BitmapFactory
+import android.media.MediaMetadataRetriever
+import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -28,6 +32,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -35,6 +41,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.androidstudy.R
 import com.example.androidstudy.ui.theme.AndroidStudyTheme
 import com.example.androidstudy.viewModel.MusicViewModel
@@ -43,6 +51,8 @@ import com.example.androidstudy.viewModel.MusicViewModel
 @Composable
 fun DetailScreen(viewModel: MusicViewModel = hiltViewModel()) {
     val musicTitle by viewModel.curTitle.collectAsState()
+    val musicUri by viewModel.curUri.collectAsState()
+
     val musicPos by viewModel.curPos.collectAsState()
     val duration by viewModel.duration.collectAsState()
     val isPlaying by viewModel.isPlaying.collectAsState()
@@ -67,10 +77,7 @@ fun DetailScreen(viewModel: MusicViewModel = hiltViewModel()) {
             .fillMaxSize()
             .background(Color(0xFFccc5b9))
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.img_panda_playing_guitar),
-            contentDescription = "Default Image"
-        )
+        AlbumCoverImage(musicUri)
 
         Row(
             modifier = Modifier
@@ -160,6 +167,7 @@ fun DetailScreen(viewModel: MusicViewModel = hiltViewModel()) {
         }
 
     }
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -216,6 +224,42 @@ fun CustomTrack(sliderValue: Float) {
                 .background(inactiveColor, CircleShape)
         )
     }
+}
+
+@Composable
+fun AlbumCoverImage(musicUri: String) {
+    val context = LocalContext.current
+    val mediaMetaDataRetriever = MediaMetadataRetriever()
+    var picture = remember(musicUri) {
+        mediaMetaDataRetriever.setDataSource(context, Uri.parse(musicUri))
+        val pic = mediaMetaDataRetriever.embeddedPicture
+        mediaMetaDataRetriever.release()
+        pic
+    }
+
+    val imageRequest = picture?.let {
+        ImageRequest.Builder(context)
+            .data(BitmapFactory.decodeByteArray(it, 0, it.size))
+            .crossfade(true)
+            .placeholder(R.drawable.img_music_default)
+            .error(R.drawable.img_music_default)
+            .listener(
+                onError = { _, result ->
+                    Log.e("=== Coil loading error :", "${result.throwable}")
+                },
+                onSuccess = { _, result ->
+                    Log.i("=== Coil loading success :", "===")
+                }
+            ).build()
+    }
+
+    AsyncImage(
+        modifier = Modifier.size(300.dp),
+        contentScale = ContentScale.Fit,
+        model = imageRequest,
+        contentDescription = null,
+        error = painterResource(R.drawable.img_music_default)
+    )
 }
 
 @Preview(showBackground = true)
